baf850f71bd8730371400a6b9a07df8a862460ff
brdbrthk <tanapon.kaj@gmail.com>
2024-05-01 18:18:47 +0700

    Add web template

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-import express from 'express';
-import chalk from 'chalk';
-import morgan from 'morgan';
-import createDebug from 'debug'
-const debug = createDebug('app')
+import express from "express";
+import chalk from "chalk";
+import morgan from "morgan";
+import createDebug from "debug";
+import path from "path";
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const debug = createDebug("app");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(morgan('combined'));
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname+ "/public/")));
 
-app.get("/", (req,res) =>{
-    res.send('Hello world');
-})
 
-app.listen(port, ()=>{
-    //debug("Listening on port " + chalk.green(port));
-    console.log("Listening on port " + chalk.blueBright(port));
+app.get("/", (req, res) => {
+  res.send("404 Not found.");
+});
 
-})
+app.listen(port, () => {
+  console.log("Listening on port " + chalk.green(port));
+});
